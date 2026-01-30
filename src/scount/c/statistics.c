e9f53b84ebd81811ae3dec8e69f93886707e4f3b
kilo52 <phil.gaiser@raven-computing.com>
2026-01-30 19:22:01 +0100

    Improved error message to give more details.
    Added the reportFileError() function to log an additional error message about
    the source file that produced an error. This gives more information to the user
    when the input is a directory, as only showing an error for the given directory
    does not make it clear which source file produced the error specifically.
    
    Signed-off-by: kilo52 <phil.gaiser@raven-computing.com>

--- src/scount/c/statistics.c
+++ src/scount/c/statistics.c
@@ -21,7 +21,28 @@
 #include "reckon/reckon.h"
 #include "scount.h"
 
-static void reportError(const char* path, RcnCountStatistics* stats) {
+static void reportFileError(
+    const RcnCountStatistics* stats,
+    const RcnSourceFile* file,
+    const RcnCountResultGroup* result
+) {
+    logE("The following file encountered an error: '%s'", file->path);
+    const char* const statErr = (
+        stats->state.errorMessage
+        ? stats->state.errorMessage
+        : ""
+    );
+    const char* const fileErr = (
+        result->state.errorMessage
+        ? result->state.errorMessage
+        : ""
+    );
+    if (strcmp(fileErr, "") != 0 && strcmp(statErr, fileErr) != 0) {
+        logE("%s (%#04x)", fileErr, result->state.errorCode); // LCOV_EXCL_LINE
+    }
+}
+
+static void reportError(const char* path, const RcnCountStatistics* stats) {
     if (stats->state.errorCode == RCN_ERR_INVALID_INPUT) {
         logE("Invalid input path: '%s'", path);
     } else {
@@ -40,6 +61,15 @@ static void reportError(const char* path, RcnCountStatistics* stats) {
             stats->state.errorCode
         );
         // LCOV_EXCL_STOP
+    }
+    if (stats->count.size > 1) {
+        for (size_t i = 0; i < stats->count.size; ++i) {
+            const RcnCountResultGroup* const result = &stats->count.results[i];
+            if (result->state.errorCode != RCN_ERR_NONE) {
+                reportFileError(stats, &stats->count.files[i], result);
+                break;
+            }
+        }
     }
 }
 
--- src/scount/tests/functionality/sh/test_scount.sh
+++ src/scount/tests/functionality/sh/test_scount.sh
@@ -154,6 +154,8 @@ function test_scount_with_stop_on_error_option() {
   assert_stdout_is_empty;
   assert_stderr_contains "An error has occurred";
   assert_stderr_contains "Syntax error detected in source code";
+  assert_stderr_contains "The following file encountered an error:";
+  assert_stderr_contains "mixedWithSyntaxError/02_has_syntax_error.c";
 }
 
 function test_scount_with_valid_directory_input_and_stop_on_error_option() {
