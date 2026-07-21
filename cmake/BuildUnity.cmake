6abd0bea10de037b075ccfb8765e4c4b9e87e3cd
Phil Gaiser <phil.gaiser@raven-computing.com>
2026-07-21 22:16:50 +0800

    Improved test suite assertion failure handling when using sanitizers.
    Added a standard Unity unity_config.h configuration header file that defines
    the UNITY_TEST_ABORT macro. The macro simply sets a global boolean flag and
    continues with the default Unity behaviour when a test assertion fails.
    
    Added the __lsan_is_turned_off() ASAN hook function to check the global
    flag and, if set, disable LSAN for the test run.
    This is so that when executing the test suite with sanitizer support
    enabled, regular test assertion failures are reported over memory leaks
    detected by ASAN. This is because a failed test assertion might result
    in cleanup code, usually located at the end of a test function, to not
    be executed, effectively triggering a spurious memory leak error.
    
    [CL]: Improved test suite assertion failure handling
    when using sanitizers. [Issue#67]
    
    Signed-off-by: Phil Gaiser <phil.gaiser@raven-computing.com>

--- cmake/BuildUnity.cmake
+++ cmake/BuildUnity.cmake
@@ -1,1 +1,7 @@
 disable_compiler_warnings(unity)
+target_compile_definitions(unity PUBLIC UNITY_INCLUDE_CONFIG_H)
+target_include_directories(
+    unity
+    PUBLIC
+    "$<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/src/lib/tests/include>"
+)
