a8284beaa130262c54f149ac09ab23ddd7d294b3
Phil Gaiser <phil.gaiser@raven-computing.com>
2026-08-08 23:08:37 +0800

    Updated Unity dependency version.
    Signed-off-by: Phil Gaiser <phil.gaiser@raven-computing.com>

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -76,6 +76,6 @@ dependency(
 dependency(
     DEPENDENCY_NAME         unity
     DEPENDENCY_RESOURCE     ThrowTheSwitch/Unity
-    DEPENDENCY_VERSION      v2.6.1
+    DEPENDENCY_VERSION      v2.7.0
     DEPENDENCY_SCOPE        TEST
 )
