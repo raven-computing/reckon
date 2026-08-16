df079268794119cd09c5c68611f3bdea591ace9a
Phil Gaiser <phil.gaiser@raven-computing.com>
2026-08-16 11:50:13 +0800

    Added support for assembly format.
    [CL]: Added support for assembly format. [#71]
    
    Signed-off-by: Phil Gaiser <phil.gaiser@raven-computing.com>

---
+++ src/scount/tests/functionality/res/mixed/sample1.s
@@ -0,0 +1,5 @@
+; First assembly sample
+section .text
+global _start
+_start:
+    mov eax, 1
---
+++ src/scount/tests/functionality/res/mixed/sample3.asm
@@ -0,0 +1,6 @@
+; Third assembly sample
+section .data
+msg db "Hello", 0
+section .text
+_start:
+    nop
