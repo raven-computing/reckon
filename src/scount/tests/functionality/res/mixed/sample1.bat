7368c9be292715eb4a88a7f2befc44ccc974cbe5
Phil Gaiser <phil.gaiser@raven-computing.com>
2026-08-25 13:36:03 +0800

    Added comments in DOS batch sample files.
    Signed-off-by: Phil Gaiser <phil.gaiser@raven-computing.com>

--- src/scount/tests/functionality/res/mixed/sample1.bat
+++ src/scount/tests/functionality/res/mixed/sample1.bat
@@ -1,4 +1,5 @@
 @echo off
+REM First DOS Batch sample
 set BUILD=release
 echo Building %BUILD%
 if "%1"=="clean" echo Cleaning
--- src/scount/tests/functionality/res/mixed/sample2.cmd
+++ src/scount/tests/functionality/res/mixed/sample2.cmd
@@ -1,5 +1,6 @@
 @echo off
 set TARGET=all
+REM Second DOS Batch sample
 if "%1"=="" set TARGET=default
 echo Target: %TARGET%
 call :done
